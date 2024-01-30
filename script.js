ddff0513f0719a8712f221cd581100ec756a9ada
djav20 <djavluis@gmail.com>
2024-01-30 12:35:12 +0100

    funcion de validacion DNI

--- script.js
+++ script.js
@@ -23,7 +23,6 @@ que contenga @ y que no sea el primero: 64
 que contenga sólo un  . y que sea después de @, 
 que no haya espacios
 numero de caracteres
-
 */
 
 function contieneArroba(email) {
@@ -41,4 +40,110 @@ function contieneArroba(email) {
   return false;
 }
 
+/**
+ * Mod23: Retorna si una letra corresponde al algoritmo
+ * @param {Int} num: numero entero 8 digitos
+ * @param {char} letra: input a comparar con mod23
+ * @return {boolean} true si letra corresponde si no false
+ */
+function mod23(num, letra){
+    let correcta_letra;
+    switch(num%23){
+        case 0:
+            correcta_letra = "T";
+            break;
+        case 1:
+            correcta_letra = "R";
+            break;
+        case 2:
+            correcta_letra = "W";
+            break;
+        case 3:
+            correcta_letra = "A";
+            break;
+        case 4:
+            correcta_letra = "G";
+            break;
+        case 5:
+            correcta_letra = "M";
+            break;
+        case 6:
+            correcta_letra = "Y";
+            break;
+        case 7:
+            correcta_letra = "F";
+            break;
+        case 8:
+            correcta_letra = "P";
+            break;
+        case 9:
+            correcta_letra = "D";
+            break;
+        case 10:
+            correcta_letra = "X";
+            break;
+        case 11:
+            correcta_letra = "B";
+            break;
+        case 12:
+            correcta_letra = "N";
+            break;
+        case 13:
+            correcta_letra = "J";
+            break;
+        case 14:
+            correcta_letra = "Z";
+            break;
+        case 15:
+            correcta_letra = "S";
+            break;
+        case 16:
+            correcta_letra = "Q";
+            break;
+        case 17:
+            correcta_letra = "V";
+            break;
+        case 18:
+            correcta_letra = "H";
+            break;
+        case 19:
+            correcta_letra = "L";
+            break;
+        case 20:
+            correcta_letra = "C";
+            break;
+        case 21:
+            correcta_letra = "K";
+            break;
+        case 22:
+            correcta_letra = "E";
+            break;
+        default:
+            correcta_letra = undefined;
+    }
 
+    return letra == correcta_letra;
+}
+
+/**
+ * Validar DNI:
+ * revisar que sean 8 numeros (entre 48 y 57 ASCII) y letra al final (9 caracteres)
+ * la letra tiene que seguir el algoritmo de module 23
+ * @param {string} DNI: numero mas letra
+ * @return {boolean} true si es valido, false si no
+ */
+function val_DNI(DNI){
+    var numero = DNI.slice(0,8);
+    var letra = DNI.slice(8,9);
+
+    var con0 = true;
+    for (let i = 0; i < numero.length; i++) {
+        con0 = con0 && numero.charCodeAt(i) >= 48 && numero.charCodeAt(i) <= 57;
+    }
+
+    var con1 = DNI.length == 9;
+    var con2 = numero.length == 8;
+    var conmod23 = mod23(parseInt(numero), letra);
+
+    return con0 && con1 && con2 && conmod23;
+}
